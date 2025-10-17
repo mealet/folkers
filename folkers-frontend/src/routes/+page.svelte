56b2258aaf78514d03fb496d253c5ec9ae04100a
mealet <jasonragonter@gmail.com>
2025-10-17 23:13:28 +0300

    added records search on main page

--- folkers-frontend/src/routes/+page.svelte
+++ folkers-frontend/src/routes/+page.svelte
@@ -1,23 +1,49 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
-	import { authGuard } from '$lib/guards/auth.guard';
 	import { PersonService } from '$lib/services/person.service';
 	import type { PersonRecord } from '$lib/types/person';
-	// import { loggedUser } from '$lib/stores/auth';
+	import Fuse from 'fuse.js';
 
 	let persons: PersonRecord[] = [];
 
+	let query = '';
+	let results: PersonRecord[] = [];
+	let fuse = Fuse<PersonRecord>;
+
 	onMount(async () => {
 		persons = await PersonService.list_persons();
+
+		fuse = new Fuse(persons, {
+			keys: ['surname', 'name', 'patronymic'],
+			threshold: 0.3
+		});
 	});
 
-	// let user = $loggedUser;
+	$: if (fuse && query.trim()) {
+		results = fuse
+			.search(query)
+			.map((result: { item: PersonRecord; refIndex: number }) => result.item);
+	} else {
+		results = persons;
+	}
 </script>
 
-<div>
-	<h3>List of persons:</h3>
+<div class="p-2">
+	<a href="/persons/create"
+		><button type="submit" class="border-1 border-black p-1">Создать</button></a
+	>
+
+	<br />
+	<br />
+
+	<input type="text" bind:value={query} placeholder="Поиск..." class="rounded border p-2" />
+
+	<br />
+	<br />
+
+	<h3>Список людей:</h3>
 	<ul>
-		{#each persons as person (person.id)}
+		{#each results as person (person.id)}
 			<li>
 				- <a href="/persons/{person.id.id.String}"
 					>{person.surname} {person.name} {person.patronymic}</a
